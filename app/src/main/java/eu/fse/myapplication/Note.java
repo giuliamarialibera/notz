f3bfda5ab7914c65fb7e274e2d40fc49155ebac6
giuliamarialibera <dottavi.giulia@gmail.com>
2018-04-12 19:00:18 +0200

    struttura progetto

---
+++ app/src/main/java/eu/fse/myapplication/Note.java
@@ -0,0 +1,8 @@
+package eu.fse.myapplication;
+
+/**
+ * Created by Amministratore on 12/04/2018.
+ */
+
+public class Note {
+}
---
+++ app/src/main/java/eu/fse/myapplication/NoteActivity.java
@@ -0,0 +1,10 @@
+package eu.fse.myapplication;
+
+import android.app.Activity;
+
+/**
+ * Created by Amministratore on 12/04/2018.
+ */
+
+public class NoteActivity extends Activity {
+}
